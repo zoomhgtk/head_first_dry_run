3689aed7b62dd6f27d39775b42fe88f726070d9d
Harry Sun <zoomhgtk@gmail.com>
2016-08-21 22:24:27 +0800

    learning how to handle exception

--- chapter3/read_sketch.py
+++ chapter3/read_sketch.py
@@ -2,13 +2,15 @@ import os
 
 print os.getcwd()
 
-os.chdir('/Users/harry/python_head_first/chapter3')
+os.chdir("/Users/harry/python_head_first/head_first_dry_run/chapter3")
 
 print os.getcwd()
 
 stream1 = open('sketch.txt')
 
-print stream1.readline(),
-print stream1.readline(),
-
+for each_line in stream1:
+	(role, line_spoken) = each_line.split(':', 1)
+	print role,
+	print "said:",
+	print line_spoken
 
